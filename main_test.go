735fae31b3e7f834e48f3de0935c19b33247ef4a
Anders Janmyr <anders@janmyr.com>
2015-04-03 08:01:28 +0200

    Fixed tests

--- main_test.go
+++ main_test.go
@@ -8,13 +8,13 @@ import (
 func TestGenerate(t *testing.T) {
 	os.Mkdir("tmp", 0777)
 	os.Chdir("tmp")
-	generate("../test", map[string]string{"NAME": "tapir"})
+	generate("../test", "tapir", map[string]string{"NAME": "tapir"})
 	os.Chdir("..")
 	os.RemoveAll("tmp")
 }
 
 func TestNewFilenameDc(t *testing.T) {
-	actual := newFilename("~/.gooserc", "prefixNAME.ccsuffix", map[string]string{"NAME": "my-name"})
+	actual := newFilename("~/.gooserc", "my-name", "prefixNAME.ccsuffix", map[string]string{"NAME": "my-name"})
 	expected := "prefixMyNamesuffix"
 	if actual != expected {
 		t.Errorf("newFilename(): %v, expected %v", actual, expected)
