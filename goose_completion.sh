8335d538e8bd5c06eff101efc22863d5a3ce23c6
Anders Janmyr <anders@janmyr.com>
2015-04-03 08:26:24 +0200

    Added cli completion

---
+++ goose_completion.sh
@@ -0,0 +1,15 @@
+_goose() {
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts="--templatedir --outputdir --verbose"
+
+    if [[ ${cur} == -* ]] ; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    fi
+}
+
+complete -o default -F _goose goose
+
