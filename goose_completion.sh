12f794cc0f51d56849398ac0e04accd2daba54fe
Anders Janmyr <anders@janmyr.com>
2015-04-05 13:10:07 +0200

    Added support for directories to goose_completion.sh

--- goose_completion.sh
+++ goose_completion.sh
@@ -1,14 +1,20 @@
 _goose() {
-    local cur prev opts
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="--templatedir --outputdir --verbose"
+  local cur prev opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  opts="--data --help --outputdir --templatedir --verbose"
 
-    if [[ ${cur} == -* ]] ; then
-        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-        return 0
-    fi
+  if [[ ${cur} == -* ]] ; then
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+  else
+    goose_dir=`ls -d ~/.goose/* | xargs -n1 basename`
+    words="$goose_dir"
+
+    COMPREPLY=($(compgen -W "$words" -- $cur))
+    return 0
+  fi
 }
 
 complete -o default -F _goose goose
