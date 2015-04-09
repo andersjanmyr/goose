68c6a54f8b37f69bd80152423dd94020d56aa2c9
Anders Janmyr <anders@janmyr.com>
2015-04-09 18:18:19 +0200

    Started with release script

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o errexit
+
+new_version=$1
+
+echoerr() { echo "$@" 1>&2; }
+
+if [ -z "$new_version" ]; then
+  echoerr "Version is required."
+  echoerr "Usage: release.sh <version>"
+  exit 1
+fi
+
+if ! git diff --quiet HEAD; then
+  echoerr "Cannot create release with a dirty repo."
+  echoerr "Commit or stash changes and try again."
+  git status -sb
+  exit 1
+fi
+
+sed -i.bak -E "s/v[0-9]\.[0-9]\.[0-9]/$new_version/g" README.md
+git add README.md
+git commit -am "Changed version to $1"
+git tag $new_version -am "Release $new_version"
+
+
