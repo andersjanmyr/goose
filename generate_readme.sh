5df8740f96f84da0818141af8ea1ef2f6c4efecf
Anders Janmyr <anders@janmyr.com>
2015-04-11 10:39:56 +0200

    Generate readme with link to this repo

--- generate_readme.sh
+++ generate_readme.sh
@@ -4,11 +4,17 @@ echo_readme() {
   echo "$@" >> ~/.goose/README.md
 }
 
-echo "Goose Templates" > ~/.goose/README.md
+cat << EOT > ~/.goose/README.md
+# Goose Templates
+
+Templates for use with the
+[Goose file generator](https://github.com/andersjanmyr/goose)
+
+EOT
 
 for d in ~/.goose/*/ ; do
   name=$(basename $d)
-  echo_readme "# $name"
+  echo_readme "## $name"
   echo_readme ''
   echo_readme '```'
   redwood $d >> ~/.goose/README.md
