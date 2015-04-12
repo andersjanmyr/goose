5b3bedec886d551eccdbf55d17885f4437f0234f
Anders Janmyr <anders@janmyr.com>
2015-04-12 09:39:36 +0200

    Added --version to bash completion

--- goose_completion.sh
+++ goose_completion.sh
@@ -3,7 +3,8 @@ _goose() {
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
-  opts="--data --force --help --interactive --outputdir --templatedir --verbose"
+  opts="--data --force --help --interactive \
+    --outputdir --templatedir --verbose --version"
 
   if [[ ${cur} == -* ]] ; then
     COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
